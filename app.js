384c1e6e18ed8a5460680af7789a74ee8b7c61af
Aleksandr M <alfie.mir@gmail.com>
2021-04-05 14:07:49 +0300

    Добавил дополнительные величины в объект SIZES. Добавили отступы и применили отступы к отрисовке в canvas

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ class Sizes {
     this.width = 600;
     this.height = 200;
     this.dpi = 2;
+    this.padding = 40;
     this.totalWidth = this.width * this.dpi;
     this.totalHeight = this.height * this.dpi;
+    this.viewHeight = this.totalHeight - this.padding * 2;
+    this.rowCount = 5;
+    this.step = this.viewHeight / this.rowCount;
   }
 }
 
@@ -32,19 +36,14 @@ function setStyles(canvas, sizes) {
  */
 function chart(canvas, data) {
   const ctx = canvas.getContext("2d");
+  // Styles
   setStyles(canvas, SIZES);
 
-  ctx.beginPath();
-  ctx.lineWidth = 4;
-  ctx.strokeStyle = '#282c35';
-  for (const [x, y] of data) {
-    /**
-     * У canvas отчет идет с верхнего левого угла. Вычитая, Я как бы меняю систему координат, начиная отсчитывать точку снизу слева, как "правильнее"
-     */
-    ctx.lineTo(x, SIZES.totalHeight - y);
-  }
-  ctx.stroke();
-  ctx.closePath();
+  // === y axis
+  drawYAxis(ctx);
+
+  // MainLine
+  drawMainLine(ctx, data);
 }
 
 chart(document.getElementById("chart"), [
@@ -56,3 +55,42 @@ chart(document.getElementById("chart"), [
   [580, 100],
   [650, 20],
 ]);
+
+/** Utils */
+function drawYAxis(ctx) {
+  ctx.beginPath();
+  ctx.strokeStyle = "#bbb";
+  ctx.font = "normal 20px Helvetica, sans-serif";
+  ctx.fillStyle = "#96a2aa";
+
+  console.log(SIZES.padding);
+
+  for (let index = 1; index <= SIZES.rowCount; index++) {
+    const yWithoutPadding = SIZES.step * index; // Тут считаем значение
+    const yWithPadding = yWithoutPadding + SIZES.padding; // Тут рисуем (визуал, а поэтому учитываем паддинги)
+
+    ctx.fillText(SIZES.totalHeight - yWithoutPadding, 5, yWithPadding - 5);
+    ctx.moveTo(0, yWithPadding);
+    ctx.lineTo(SIZES.totalWidth, yWithPadding);
+  }
+
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function drawMainLine(ctx, data) {
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = "#282c35";
+
+  for (const [x, y] of data) {
+    const xTotalCoord = SIZES.totalHeight - SIZES.padding;
+    /**
+     * У canvas отчет идет с верхнего левого угла. Вычитая, Я как бы меняю систему координат, начиная отсчитывать точку снизу слева, как "правильнее"
+     */
+    ctx.lineTo(x, xTotalCoord - y);
+  }
+
+  ctx.stroke();
+  ctx.closePath();
+}
