36f941fe3fe7fee4b4d12809869cd5ca13ef172f
Aleksandr M <alfie.mir@gmail.com>
2021-04-05 13:39:02 +0300

    Создал папку src, в которой создал файл utils. Вынес создание переменной SIZES в отдельный класс, чтобы не терять контекст

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-const SIZES = {
-  width: 600,
-  height: 200,
-  dpi: 2
-};
+// import { setStyles, SIZES } from "./src/utils";
 
+class Sizes {
+  constructor() {
+    this.width = 600;
+    this.height = 200;
+    this.dpi = 2;
+    this.totalWidth = this.width * this.dpi;
+    this.totalHeight = this.height * this.dpi;
+  }
+}
+
+const SIZES = new Sizes();
+
+/**
+ * Функция применения стилей к канвасу. Размеры, разрешение и т.д.
+ * @param {HTMLCanvasElement} canvas
+ * @param {Number} dpi
+ */
+function setStyles(canvas, sizes) {
+  const { width, height, totalWidth, totalHeight } = sizes;
+  canvas.style.width = width + "px";
+  canvas.style.height = height + "px";
+  canvas.width = totalWidth;
+  canvas.height = totalHeight;
+}
+
+/**
+ * Основная функция инициализации Canvas
+ * @param {HTMLCanvasElement} canvas
+ * @param {Array[]} data
+ */
 function chart(canvas, data) {
   const ctx = canvas.getContext("2d");
-  setStyles(canvas, SIZES.dpi);
+  setStyles(canvas, SIZES);
+
+  ctx.beginPath();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = '#282c35';
+  for (const [x, y] of data) {
+    /**
+     * У canvas отчет идет с верхнего левого угла. Вычитая, Я как бы меняю систему координат, начиная отсчитывать точку снизу слева, как "правильнее"
+     */
+    ctx.lineTo(x, SIZES.totalHeight - y);
+  }
+  ctx.stroke();
+  ctx.closePath();
 }
 
-function setStyles(canvas, dpi) {
-  canvas.style.width = SIZES.width + "px";
-  canvas.style.height = SIZES.height + "px";
-  canvas.width = SIZES.width * dpi;
-  canvas.height = SIZES.height * dpi;
-}
-
-chart(document.getElementById("chart"));
+chart(document.getElementById("chart"), [
+  [0, 0],
+  [200, 200],
+  [400, 100],
+  [450, 120],
+  [550, 80],
+  [580, 100],
+  [650, 20],
+]);
